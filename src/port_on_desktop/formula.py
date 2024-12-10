77145895a4979d910891716c147564685389ad4a
Nikita Kozmin <kozmin2100@gmail.com>
2024-12-10 20:00:41 +0300

    Add check

--- src/port_on_desktop/formula.py
+++ src/port_on_desktop/formula.py
@@ -1,4 +1,9 @@
-def calculate_proximity_percentage(choices, saddle_value, min_num, max_num):
+def calculate_proximity_percentage(choices, saddle_value, min_num=-99, max_num=99):
+    # проверка на очевидно неправильный ввод
+    max_choice = max(choices, default=0)
+    min_choice = min(choices, default=0)
+    if max_choice > max_num or min_choice < min_num:
+        raise ValueError("Incorrectly sent parameters min or max num")
     # вычисляем расстояние от седловой точки для каждого выбора
     distances = [abs(choice - saddle_value) for choice in choices]
     # максимальное возможное расстояние
--- src/port_on_desktop/main.py
+++ src/port_on_desktop/main.py
@@ -1,4 +1,7 @@
 from matrix_generations import *
+from formula import *
+
 
 if __name__ == "__main__":
+    print(calculate_proximity_percentage([], 3, -5, 5))
     
