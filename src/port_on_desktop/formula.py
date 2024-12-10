77fd2442f365a611855eab3651d8a36b440835a2
Kopasova Kseniya <139976614+typa1yaaa@users.noreply.github.com>
2024-12-10 21:48:18 +0300

    update formula + check saddle_value

--- src/port_on_desktop/formula.py
+++ src/port_on_desktop/formula.py
@@ -4,12 +4,18 @@ def calculate_proximity_percentage(choices, saddle_value, min_num=-99, max_num=9
     min_choice = min(choices, default=0)
     if max_choice > max_num or min_choice < min_num:
         raise ValueError("Incorrectly sent parameters min or max num")
-    # вычисляем расстояние от седловой точки для каждого выбора
-    distances = [abs(choice - saddle_value) for choice in choices]
-    # максимальное возможное расстояние
-    max_distance = abs(max_num - min_num)
-    # среднее расстояние
-    avg_distance = sum(distances) / len(distances) if distances else 0
-    # вычисляем процент близости
-    proximity_percentage = (1 - avg_distance / max_distance) * 100
-    return max(0, proximity_percentage)  # Ограничиваем процент от 0 до 100
+    # проверка на седловую точку
+    if saddle_value > max_num or saddle_value < min_num:
+        raise ValueError("Incorrectly sent parameter saddle_value")
+    # максимальное возможное расстояние    
+    max_distance1 = abs(saddle_value - min_num)
+    max_distance2 = abs(max_num - saddle_value)
+    max_possible_sum = received_sum = 0
+    for i in range(len(choices)):
+        if choices[i] <= saddle_value:
+            max_possible_sum += max_distance1
+            received_sum += max_distance1 - abs(choices[i]-saddle_value)
+        else:
+            max_possible_sum += max_distance2
+            received_sum += max_distance2 - abs(choices[i]-saddle_value)
+    return (received_sum/max_possible_sum) * 100 
